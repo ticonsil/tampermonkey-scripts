a60461469d1bd09a9fd432cd44b2ac5850784cfb
Fiscalzeroseis <fiscal06@consilcontabilidade.com>
2026-06-08 10:38:35 -0300

    fix: URL de download do script corrigida

--- acessorias/Gestão de Processos/onboardingEmMassa.user.js
+++ acessorias/Gestão de Processos/onboardingEmMassa.user.js
@@ -6,8 +6,8 @@
 // @author       TIConsil
 // @match        https://app.acessorias.com/sysmain.php?m=125*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/ticonsil/tampermonkey-scripts/main/onboardingEmMassa.user.js
-// @updateURL    https://raw.githubusercontent.com/ticonsil/tampermonkey-scripts/main/onboardingEmMassa.user.js
+// @downloadURL  https://github.com/ticonsil/tampermonkey-scripts/raw/refs/heads/main/acessorias/Gest%C3%A3o%20de%20Processos/onboardingEmMassa.user.js
+// @updateURL    https://github.com/ticonsil/tampermonkey-scripts/raw/refs/heads/main/acessorias/Gest%C3%A3o%20de%20Processos/onboardingEmMassa.user.js
 // ==/UserScript==
 (function () {
   'use strict';
